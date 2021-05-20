efd2b28e59f8c3a1d38d04a40a62aba9de131f52
Daniel Fabián <72435753+Danny-06@users.noreply.github.com>
2021-05-20 09:48:15 +0200

    Add files via upload

--- Tienda Informática.sql
+++ Tienda Informática.sql
@@ -14,25 +14,25 @@ CREATE TABLE productos (
 INSERT INTO productos
 VALUES
 
-(1,  'Televisión', 'television.png', 630),
-(2,  'Móvil',      'movil.webp', 250),
-(3,  'Portátil', 'portatil.jpg', 1300),
-(4,  'Teclado', 'teclado.jpg', 15),
+(1,  'Televisión',       'television.png',       630),
+(2,  'Móvil',            'movil.webp',           250),
+(3,  'Portátil',         'portatil.jpg',        1300),
+(4,  'Teclado',          'teclado.jpg',           15),
 
-(5,  'Ratón', 'raton.jpg', 12),
-(6,  'Cascos', 'cascos.jpg', 10),
-(7,  'Silla Gaming', 'silla-gaming.jpg', 450),
+(5,  'Ratón',            'raton.jpg',             12),
+(6,  'Cascos',           'cascos.jpg',            10),
+(7,  'Silla Gaming',     'silla-gaming.jpg',     450),
 (8,  'Realidad Virtual', 'realidad-virtual.jpg', 310),
 
-(9,  'Tablet', 'tablet.jpg', 200),
-(10, 'Altavoces', 'altavoces.jpg', 50),
-(11, 'Webcam', 'webcam.jpg', 10),
-(12, 'Tarjeta Gráfica', 'tarjeta-grafica.jpg', 1560),
+(9,  'Tablet',           'tablet.jpg',           200),
+(10, 'Altavoces',        'altavoces.jpg',         50),
+(11, 'Webcam',           'webcam.jpg',            10),
+(12, 'Tarjeta Gráfica',  'tarjeta-grafica.jpg', 1560),
 
-(13, 'Procesador', 'procesador.jpg', 1000),
-(14, 'Memoria Ram', 'memoria-ram.jpg', 500),
-(15, 'Disco SSD', 'disco-ssd.jpg', 790),
-(16, 'Reloj Avanzado', 'reloj-avanzado.jpg', 60)
+(13, 'Procesador',       'procesador.jpg',      1000),
+(14, 'Memoria Ram',      'memoria-ram.jpg',      500),
+(15, 'Disco SSD',        'disco-ssd.jpg',        790),
+(16, 'Reloj Avanzado',   'reloj-avanzado.jpg',    60)
 
 
 
