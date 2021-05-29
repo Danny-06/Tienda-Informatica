2ebe6a8bc90f89ac5c33e7e698e8ab690c8eef5c
Daniel Fabián <72435753+Danny-06@users.noreply.github.com>
2021-05-29 15:44:17 +0200

    Update Tienda Informática.sql

--- Tienda Informática.sql
+++ Tienda Informática.sql
@@ -1,17 +1,19 @@
-CREATE DATABASE TIENDA
+CREATE DATABASE tienda;
+
+USE tienda;
 
 
-CREATE TABLE productos (
+CREATE TABLE producto (
   id INT NOT NULL,
   nombre VARCHAR(50) NOT NULL,
   nombreArchivo VARCHAR(50) NOT NULL,
-  precio INT NOT NULL
+  precio INT NOT NULL,
 
   PRIMARY KEY(id)
-)
+);
 
 
-INSERT INTO productos
+INSERT INTO producto
 VALUES
 
 (1,  'Televisión',       'television.png',       630),
@@ -32,30 +34,28 @@ VALUES
 (13, 'Procesador',       'procesador.jpg',      1000),
 (14, 'Memoria Ram',      'memoria-ram.jpg',      500),
 (15, 'Disco SSD',        'disco-ssd.jpg',        790),
-(16, 'Reloj Avanzado',   'reloj-avanzado.jpg',    60)
+(16, 'Reloj Avanzado',   'reloj-avanzado.jpg',    60);
 
 
 
 CREATE TABLE registro (
   id INT NOT NULL,
   idProducto INT NOT NULL,
-  nombreProducto VARCHAR(20) NOT NULL,
-  precioProducto INT NOT NULL,
   cantidadProducto INT NOT NULL,
 
   PRIMARY KEY(id, idProducto),
-  FOREIGN KEY (idProducto) REFERENCES productos(id)
-)
+  FOREIGN KEY (idProducto) REFERENCES producto(id)
+);
 
 
 INSERT INTO registro
 VALUES
 
-(1, 1,  'Televisión',      630,   1),
-(1, 9,  'Tablet',          200,   2),
-(2, 6,  'Cascos',          10,    1),
-(2, 14, 'Memoria Ram',     500,   4),
-(3, 13, 'Procesador',      1000,  2),
-(3, 16, 'Reloj Avanzado',  60,   10),
-(3, 15, 'Disco SSD',       790,   7),
-(4, 10, 'Altavoces',       50,    5)
+(1, 1,   1),
+(1, 9,   2),
+(2, 6,   1),
+(2, 14,  4),
+(3, 13,  2),
+(3, 16, 10),
+(3, 15,  7),
+(4, 10,  5);
